51d162c0b4c5db64af47c15552031f1db0d9a999
yeetsquad747 <74121237+yeetsquad747@users.noreply.github.com>
2022-07-03 12:12:36 +0530

    Adding code comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Imports
 from random import uniform, choice, randint
 from variables.ores import ores
 from variables.shop import shopItems
@@ -10,30 +11,40 @@ from database import Database
 import sys
 from animations.load_animation import load_animation
 
+# Clears the screen
 console = Console()
 console.clear()
 
+# Ores that are obtained are picked from this list
 obtainableOres = []
+
+# Json based database
 db = Database("database.json")
 try:
     userInventory = db.printdb()
 except:
     userInventory = {}
 
-
+# Function to get random splash text
 def get_random_line(file_name):
     line = choice(open(file_name).readlines())
     return line
 
-
+# Main menu
 def main_menu():
+    # Splash Screen
     splash_screen = text2art("LordOfTheMines")
+
+    # Colored Splash Screen
     rgb_splash_screen = Colorate.Diagonal(Colors.blue_to_red, splash_screen)
 
     print(rgb_splash_screen)
+
+    # Random splash text
     splash_text = get_random_line("variables/splash.txt")
     rprint(f"[white]{splash_text}[/white]")
 
+    # Asks questions to the user
     rprint("[blue]What would you like to do?[/blue]")
     rprint("[green]1. Mine[/green]")
     rprint("[green]2. Sell[/green]")
@@ -42,26 +53,32 @@ def main_menu():
     rprint("[green]5. Info[/green]")
     userInput = input()
     console = Console()
+    # Mine
     if userInput == "1" or userInput.lower() == "mine":
         while True:
             console.clear()
             return mine()
+    # Sell
     elif userInput == "2" or userInput.lower() == "sell":
         while True:
             console.clear()
             return sell()
+    # Shop
     elif userInput == "3" or userInput.lower() == "shop":
         while True:
             console.clear()
             return shop()
+    # Inventory
     elif userInput == "4" or userInput.lower() == "inventory":
         while True:
             console.clear()
             return inventory()
+    # Info
     elif userInput == "5" or userInput.lower == "info":
         while True:
             console.clear()
             return info()
+    # Invalid input
     else:
         rprint("[red]Invalid input[/red]")
         main_menu()
@@ -73,49 +90,60 @@ def mine():
             Picks an ore
             # TODO add more ores
         """
+        # Random number
         chance = round(uniform(0, 1), 2)
+
+        # Asks user to type random number(To prove they are not afk)
         inputToType = randint(1, 100)
         rprint(f"Type the number {inputToType}(or type exit to exit)")
         userInput = input()
         recieved_ore = []
+        # Checks if what they typed is the random number
         if userInput == str(inputToType):
+            # Puts all obtainable ores into a list
             for ore in ores:
                 if ores[ore]["chance"] >= chance:
                     obtainableOres.append(ore)
             try:
+                # Picks an ore from the list
                 recieved_ore = choice(obtainableOres)
             except:
+                # When list is empty
                 rprint("[red]You didn't recieve any ore[/red]")
                 return mine()
+            # Gives ore to user
             try:
                 userInventory[recieved_ore] += 1
             except:
                 userInventory[recieved_ore] = 1
+            # If user gets a legendary ore
             if ores[recieved_ore]["type"] == "legendary":
                 msg = Colorate.DiagonalBackwards(Colors.rainbow, f"You found some {recieved_ore}!")
                 print(msg)
                 return mine()
+            # If user gets a normal ore
             print(f"""You got a {recieved_ore}""")
-            console = Console()
+        # Exit
         elif userInput == "exit":
-            console = Console()
             console.clear()
             return main_menu()
+        # If user doesn't type specified number
         else:
             rprint(f"You were supposed to type {inputToType}")
-
 
 def sell():
     """
         Sell a certain amount of ores
     """
     while True:
+        # Inventory table
         table = Table(title="Inventory")
 
         table.add_column("Ore", justify="right", style="green", no_wrap=True)
         table.add_column("Price", justify="right", style="blue")
         table.add_column("Amount", justify="right", style="magenta")
 
+        # Adds all the elements to inventory table
         for ore in userInventory:
             if ore == "coins":
                 continue
@@ -129,27 +157,29 @@ def sell():
                 except:
                     table.add_row(
                         f"""{ore}""", f"""{shopItems[ore]["price"]}""", f"""{userInventory[ore]}""")
-
-        console = Console()
+        # Prints the table
         console.print(table)
 
+        # Coins
         try:
             rprint(f"""Coins: {userInventory["coins"]}""")
         except:
             userInventory["coins"] = 0
             rprint(f"""Coins: 0""")
 
+        # Asks what to sell
         rprint("[magenta]What ore would you like to sell?[/magenta](type exit to exit and type sellall to sell everything)")
 
+        # Gets user input
         userInput = input()
 
-        console = Console()
-
+        # If user wants to exit
         if userInput == "exit":
             console.clear()
             return main_menu()
 
-        if userInput == "sellall":
+        # If user wants to sell everything
+        elif userInput == "sellall":
             console.clear()
             for ore in userInventory:
                 if ore == "coins":
@@ -166,21 +196,26 @@ def sell():
             input()
             return main_menu()
 
-        if userInput.lower() not in userInventory.keys():
+        # If ore is invalid
+        elif userInput.lower() not in userInventory.keys():
             return rprint(f"[red]{userInput} is not a valid ore[/red]")
 
-        if userInventory[userInput.lower()] == 0:
+        # If they don't have enough of an ore to sell
+        elif userInventory[userInput.lower()] == 0:
             return rprint(f"""[red]You don't enough {userInput.lower()} to sell[/red]""")
 
+        # If they try to sell coins
         elif userInput.lower() == "coins":
             return rprint(f"[red]You can't sell coins[/red]")
 
         ore_to_sell = userInput
 
+        # How much does user want to sell
         rprint(
             f"[magenta]How many {ore_to_sell} would you like to sell(type exit to exit)?[/magenta]")
         userInput = input()
 
+        # Again they can type exit
         if(userInput == "exit"):
             return main_menu()
 
@@ -189,11 +224,15 @@ def sell():
         except:
             return rprint(f"[red]{userInput} is not a valid number[/red]")
 
+        # If they don't have enough of an ore to sell
         if userInput > userInventory[ore_to_sell]:
             return rprint(f"[red]You don't have enough {ore_to_sell} to sell[/red]")
+        
+        # To prevent them from selling negative amounts
         elif userInput < 0:
             return rprint(f"[red]You can't sell negative amounts of ore[/red]")
 
+        # Actually selling the ore
         userInventory[ore_to_sell] -= userInput
         userInventory["coins"] += userInput * ores[ore_to_sell]["price"]
 
@@ -201,19 +240,21 @@ def sell():
 
         for ore in userInventory:
             if userInput == ore.lower():
-                userInventory[ore] = userInventory[ore] - 1
-                userInventory["coins"] = userInventory["coins"] + 1
+                userInventory[ore] -= 1
+                userInventory["coins"] += 1
                 rprint(f"[orange] You sold {1} {ore}! [orange]")
 
-
+# Inventory
 def inventory():
     while True:
+        # Inventory table
         table = Table(title="Inventory")
 
         table.add_column("Ore", justify="right", style="green", no_wrap=True)
         table.add_column("Price", justify="right", style="blue")
         table.add_column("Amount", justify="right", style="magenta")
 
+        # Adds all the elements to inventory table
         for ore in userInventory:
             if ore == "coins":
                 continue
@@ -228,8 +269,10 @@ def inventory():
                     table.add_row(
                         f"""{ore}""", f"""{shopItems[ore]["price"]}""", f"""{userInventory[ore]}""")
 
-        console = Console()
+        # Prints the table
         console.print(table)
+
+        # Coins
         try:
             rprint(f"""Coins: {userInventory["coins"]}""")
         except:
@@ -241,11 +284,17 @@ def inventory():
 
 
 def shop():
+    """
+        Shop
+        # TODO
+    """
+    # Shop table
     table = Table(title="Shop")
 
     table.add_column("Item", justify="right", style="green", no_wrap=True)
     table.add_column("Price", justify="right", style="blue")
 
+    # Adds all the elements to shop table
     for item in shopItems:
         try:
             table.add_row(
@@ -254,11 +303,15 @@ def shop():
             table.add_row(f"""{item}""", f"""Priceless""")
 
     console.log(table)
+
+    # Coins
     try:
         rprint(f"Coins: {userInventory['coins']}")
     except:
         userInventory["coins"] = 0
-        rprint(f"Coins: 0")
+        rprint(f"Coins: 0") 
+
+    # Asks what to buy
     print("What would you like to buy(type exit to exit)")
     userInput = input()
     for item in shopItems:
@@ -275,12 +328,14 @@ def shop():
             else:
                 rprint(f"[red]You don't have enough coins to buy {item}[/red]")
             return shop()
+    # Exit
     if userInput.lower() == "exit":
         return main_menu()
 
-
+# Info
 def info():
     while True:
+        # Asks user what they would like to see info about
         rprint("[green]What item would you like to see more about(type exit to exit)?([/green]")
 
         userInput = input()
@@ -315,7 +370,7 @@ def info():
             rprint(f"[red]{userInput} is not a valid item[/red]")
             return info()
 
-
+# Error handling
 if __name__ == "__main__":
     try:
         while True:
